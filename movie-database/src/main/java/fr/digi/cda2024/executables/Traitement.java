592575971700cf215eb570075f86896d14223ec9
Erico Labare <erico.labare@outlook.com>
2024-11-07 17:08:22 +0100

    test branch

--- movie-database/src/main/java/fr/digi/cda2024/executables/Traitement.java
+++ movie-database/src/main/java/fr/digi/cda2024/executables/Traitement.java
@@ -2,6 +2,6 @@ package fr.digi.cda2024.executables;
 
 public class Traitement {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world !");
     }
 }
