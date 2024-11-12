b757b0cd5abd5641ac8a461be3495d270c8e9431
popobg <pauline.bouyssou1@gmail.com>
2024-11-12 22:05:35 +0100

    Nettoyage du dossier

--- movie-database/src/main/java/fr/digi/cda2024/ihm/Remplissage.java
+++ movie-database/src/main/java/fr/digi/cda2024/ihm/Remplissage.java
@@ -1,5 +1,6 @@
 package fr.digi.cda2024.ihm;
 
+import fr.digi.cda2024.dal.PersistenceManager;
 import fr.digi.cda2024.dto.FilmDTO;
 import fr.digi.cda2024.entite.Film;
 import fr.digi.cda2024.services.DtoEntitesMapper;
@@ -7,6 +8,7 @@ import fr.digi.cda2024.services.FileTools;
 import fr.digi.cda2024.services.JsonParser;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 import java.io.File;
@@ -20,6 +22,7 @@ public class Remplissage {
         // Récupération du chemin vers le fichier JSON auprès de l'utilisateur
         File file = FileTools.getJsonFileFromUser();
 
+        // Parsing du JSON vers les classes DTO
         FilmDTO[] filmsDTO = null;
         try {
             filmsDTO = JsonParser.JsonToDotParse(file);
@@ -29,25 +32,30 @@ public class Remplissage {
             System.out.println(e.getMessage());
         }
 
+        // Mapping des classes DTO vers des classes POJO entités
         Set<Film> filmsEntites = new HashSet<>();
-
         if (filmsDTO != null) {
             DtoEntitesMapper mapper = new DtoEntitesMapper();
             filmsEntites = mapper.mapDtoVersEntites(filmsDTO);
             System.out.println("Mapping Entités OK");
         }
 
-        // Persistance des films en base de données
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("movies");
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
+        // Persistance des données des classes entités en base de données
+        // Création d'un EntityManagerFactory à l'aide d'une classe utilitaire
+        EntityManagerFactory emf = PersistenceManager.getEntityManagerFactory();
 
-        for (Film film: filmsEntites) {
-            em.persist(film);
+        try(EntityManager em = emf.createEntityManager()) {
+            EntityTransaction transaction = em.getTransaction();
+            transaction.begin();
+
+
+            // Configuration en cascade de la persistance des entités
+            for (Film film : filmsEntites) {
+                em.persist(film);
+            }
+
+            transaction.commit();
         }
-
-        em.getTransaction().commit();
-        em.close();
-        emf.close();
+        PersistenceManager.closeEntityManagerFactory();
     }
 }
